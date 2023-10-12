361a274751301526c9f372ec0c7e074ead52e3f5
Salman K.A.A <40920317+Salman2301@users.noreply.github.com>
2023-10-13 00:02:24 +0200

    fix: add comment to sample file

--- src/sample-file.js
+++ src/sample-file.js
@@ -1,5 +1,9 @@
 // sample-file.js
 
+// GITHUB   : https://github.com/Salman2301/code-snippet-template/blob/master/src/sample-file.js
+// PUBLISHED: https://salman2301.github.io/code-snippet-template/sample-file.html
+// Any files that are not `*.config.json` in the `src` folder generated `*.html` to it's respective path.
+
 // Function to calculate the square of a number
 function calculateSquare(number) {
   return number * number;
